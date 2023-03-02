cec2a7c9117cf0ce02666bdd3a2b82a3ccab9a5f
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-03-02 22:16:41 +0800

    Create a function that will initialize the first item's id to search params & Add select notebook handler

--- src/components/NotebookNav/NotebookNav.tsx
+++ src/components/NotebookNav/NotebookNav.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import { useModalContext } from "../../contexts/modalCtx";
 import scss from "./notebookNav.module.scss";
 import { HiPlus } from "react-icons/hi";
@@ -8,6 +9,21 @@ import { useNotebookContext } from "../../contexts/notebookCtx";
 const NotebookNav = () => {
     const { state, openModal } = useModalContext()!;
     const { notebooks } = useNotebookContext();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const notebookId = searchParams.get("notebookId");
+
+    useEffect(() => {
+        if (notebookId === "undefined" || !notebookId) {
+            const firstNotebookId = notebooks[0]?.id;
+
+            setSearchParams({ notebookId: firstNotebookId });
+        }
+    }, [notebookId]);
+
+    const selectNotebookHandler = (id: string) => {
+        setSearchParams({ notebookId: id });
+    };
 
     return (
         <nav>
@@ -17,8 +33,11 @@ const NotebookNav = () => {
             {notebooks.map(({ id, name }) => (
                 <button
                     key={id}
-                    className={scss.active}
+                    className={`${scss["notebook-btn"]} ${
+                        notebookId === id ? scss.active : ""
+                    }`}
                     id={scss["note-title"]}
+                    onClick={() => selectNotebookHandler(id)}
                 >
                     {name}
                 </button>
