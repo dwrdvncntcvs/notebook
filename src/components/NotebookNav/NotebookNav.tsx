9c56588d48aa3e49875fca9951d97ca482de191b
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-03-02 21:31:07 +0800

    Display list of notebooks as buttons

--- src/components/NotebookNav/NotebookNav.tsx
+++ src/components/NotebookNav/NotebookNav.tsx
@@ -3,18 +3,26 @@ import { useModalContext } from "../../contexts/modalCtx";
 import scss from "./notebookNav.module.scss";
 import { HiPlus } from "react-icons/hi";
 import CreateModal from "../CreateNotebook/CreateModal";
+import { useNotebookContext } from "../../contexts/notebookCtx";
 
 const NotebookNav = () => {
     const { state, openModal } = useModalContext()!;
+    const { notebooks } = useNotebookContext();
 
     return (
         <nav>
             <button id={scss["float-btn"]} onClick={() => openModal()}>
                 <HiPlus />
             </button>
-            <button className={scss.active} id={scss["note-title"]}>
-                Sample Note
-            </button>
+            {notebooks.map(({ id, name }) => (
+                <button
+                    key={id}
+                    className={scss.active}
+                    id={scss["note-title"]}
+                >
+                    {name}
+                </button>
+            ))}
 
             {state === "open" ? <CreateModal /> : null}
         </nav>
