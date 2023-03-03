e797ce8a7a2d7c7c2d6e57a4aca1ef4c56ccee89
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-03-03 22:12:11 +0800

    Display all pages

--- src/components/PagesAside/PagesAside.tsx
+++ src/components/PagesAside/PagesAside.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HiPlus } from "react-icons/hi";
+import { useSearchParams } from "react-router-dom";
 import { MODAL, useModalContext } from "../../contexts/modalCtx";
+import { usePageContext } from "../../contexts/pageCtx";
 import CreatePage from "../CreatePage/CreatePage";
 import scss from "./pagesAside.module.scss";
 
 const PagesAside = () => {
     const { openModal, name } = useModalContext()!;
+    const { pages, notebookId } = usePageContext();
+    const [searchParams, setSearchParams] = useSearchParams();
 
     const createPageHandler = () => {
         openModal(MODAL.CREATE_PAGE);
     };
+
+    const pageId = searchParams.get("page");
+
+    useEffect(() => {
+        const p_id = pages[0]?.id;
+        setSearchParams({ notebookId, page: p_id });
+    }, [pages]);
 
     return (
         <aside>
@@ -19,8 +30,11 @@ const PagesAside = () => {
                 </button>
             </div>
             <div className={scss.pages}>
-                <button className={scss.active}>Page 1</button>
-                <button>Page 1</button>
+                {pages.map(({ id, name }) => (
+                    <button className={pageId === id ? scss.active : ""} key={id}>
+                        {name}
+                    </button>
+                ))}
             </div>
 
             {name === MODAL.CREATE_PAGE ? <CreatePage /> : null}
