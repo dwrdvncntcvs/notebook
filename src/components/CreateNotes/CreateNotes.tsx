03c4cf28f86b6f38050e7bda4eba5c5c11a0b333
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-03-03 23:10:28 +0800

    Modify Create note component and add states and submit method

--- src/components/CreateNotes/CreateNotes.tsx
+++ src/components/CreateNotes/CreateNotes.tsx
@@ -1,15 +1,35 @@
-import React from "react";
-import scss from "./createNotes.module.scss"
+import React, { useState, ChangeEvent } from "react";
+import { HiPlus } from "react-icons/hi";
+import { toast } from "react-toastify";
+import scss from "./createNotes.module.scss";
 
 const CreateNotes = () => {
+    const [note, setNote] = useState("");
+
+    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setNote(e.target.value);
+    };
+
+    const submitHandler = () => {
+        if (!note)
+            toast.error("Can't submit without note", { position: "top-right" });
+
+        console.log("Note: ", note);
+        setNote("");
+    };
+
     return (
-        <section className={scss['create-notes']}>
+        <section className={scss["create-notes"]}>
             <textarea
-                name=""
-                id=""
+                name="note"
+                id="note"
                 placeholder="Enter notes here..."
+                onChange={onChangeHandler}
+                value={note}
             ></textarea>
-            <button>Add</button>
+            <button onClick={submitHandler}>
+                <HiPlus /> Note
+            </button>
         </section>
     );
 };
