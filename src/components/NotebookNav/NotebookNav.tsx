f41151a1b25b0f027c3ac16a181dacff852b5855
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-03-06 20:54:01 +0800

    Set the default notebookId parameter on notebook context rather than on notebook nav component

--- src/components/NotebookNav/NotebookNav.tsx
+++ src/components/NotebookNav/NotebookNav.tsx
@@ -8,18 +8,8 @@ import { useNotebookContext } from "../../contexts/notebookCtx";
 
 const NotebookNav = () => {
     const { name, openModal } = useModalContext()!;
-    const { notebooks } = useNotebookContext();
+    const { notebooks, notebookId } = useNotebookContext();
     const [searchParams, setSearchParams] = useSearchParams();
-
-    const notebookId = searchParams.get("notebookId");
-
-    useEffect(() => {
-        if (notebookId === "undefined" || !notebookId) {
-            const firstNotebookId = notebooks[0]?.id;
-
-            setSearchParams({ notebookId: firstNotebookId });
-        }
-    }, [notebookId]);
 
     const selectNotebookHandler = (id: string) => {
         setSearchParams({ notebookId: id });
--- src/contexts/notebookCtx.tsx
+++ src/contexts/notebookCtx.tsx
@@ -7,24 +7,30 @@ import {
     useEffect,
     useCallback,
 } from "react";
+import { useSearchParams } from "react-router-dom";
 import { Notebook } from "../models/Notebook";
 import NotebookService from "../services/notebook";
 
 interface NotebookData {
     notebooks: Notebook[];
     createNotebook: (notebook: Notebook) => void;
+    notebookId: string;
 }
 
 const notebookData: NotebookData = {
     notebooks: [],
     createNotebook(notebook: Notebook) {},
+    notebookId: "",
 };
 
 const NotebookContext = createContext<NotebookData>(notebookData);
 
 const NotebookProvider: FC<PropsWithChildren> = ({ children }) => {
     const [notebooks, setNotebooks] = useState<Notebook[]>([]);
+    const [searchParams, setSearchParams] = useSearchParams();
     const notebookService = new NotebookService();
+
+    const notebookId = searchParams.get("notebookId") as string;
 
     const getAllNotebooks = useCallback(() => {
         const notebooks = notebookService.getAll();
@@ -35,13 +41,23 @@ const NotebookProvider: FC<PropsWithChildren> = ({ children }) => {
         getAllNotebooks();
     }, [getAllNotebooks]);
 
+    useEffect(() => {
+        if (notebookId === "undefined" || !notebookId) {
+            const firstNotebookId = notebooks[0]?.id;
+
+            setSearchParams({ notebookId: firstNotebookId });
+        }
+    }, [notebookId]);
+
     const createNotebook = (notebook: Notebook) => {
         notebookService.create(notebook);
         getAllNotebooks();
     };
 
     return (
-        <NotebookContext.Provider value={{ notebooks, createNotebook }}>
+        <NotebookContext.Provider
+            value={{ notebooks, createNotebook, notebookId }}
+        >
             {children}
         </NotebookContext.Provider>
     );
