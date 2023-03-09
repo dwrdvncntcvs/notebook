5f1d98aacda106cef83ea7dcd926d59d82a7bae4
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-03-09 19:06:12 +0800

    Implement reducer on notebook context

--- src/contexts/notebookCtx.tsx
+++ src/contexts/notebookCtx.tsx
@@ -3,9 +3,9 @@ import {
     FC,
     PropsWithChildren,
     useContext,
-    useState,
     useEffect,
     useCallback,
+    useReducer,
 } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Notebook } from "../models/Notebook";
@@ -27,10 +27,61 @@ const notebookData: NotebookData = {
     notebookId: "",
 };
 
+interface NotebookState {
+    notebooks: Notebook[];
+    notebookId: string;
+}
+
+type Action =
+    | {
+          type: "setNotebooks";
+          payload: Notebook[];
+      }
+    | {
+          type: "setNotebookId";
+          payload: string;
+      }
+    | {
+          type: "addNotebook";
+          payload: Notebook;
+      }
+    | {
+          type: "deleteNotebook";
+          payload: string;
+      };
+
+const notebookState: NotebookState = {
+    notebooks: [],
+    notebookId: "",
+};
+
+const notebookReducer = (state: NotebookState, action: Action) => {
+    switch (action.type) {
+        case "setNotebooks":
+            return { ...state, notebooks: action.payload };
+        case "setNotebookId":
+            return { ...state, notebookId: action.payload };
+        case "addNotebook":
+            return {
+                ...state,
+                notebooks: [...state.notebooks, action.payload],
+            };
+        case "deleteNotebook":
+            return {
+                ...state,
+                notebooks: state.notebooks.filter(
+                    ({ id }) => id !== action.payload
+                ),
+            };
+        default:
+            return state;
+    }
+};
+
 const NotebookContext = createContext<NotebookData>(notebookData);
 
 const NotebookProvider: FC<PropsWithChildren> = ({ children }) => {
-    const [notebooks, setNotebooks] = useState<Notebook[]>([]);
+    const [state, dispatch] = useReducer(notebookReducer, notebookState);
     const [searchParams, setSearchParams] = useSearchParams();
     const notebookService = new NotebookService();
 
@@ -38,7 +89,7 @@ const NotebookProvider: FC<PropsWithChildren> = ({ children }) => {
 
     const getAllNotebooks = useCallback(() => {
         const notebooks = notebookService.getAll();
-        setNotebooks(notebooks);
+        dispatch({ type: "setNotebooks", payload: notebooks });
     }, []);
 
     useEffect(() => {
@@ -47,33 +98,34 @@ const NotebookProvider: FC<PropsWithChildren> = ({ children }) => {
 
     useEffect(() => {
         if (notebookId === "undefined" || !notebookId) {
-            const firstNotebookId = notebooks[0]?.id;
-
+            const firstNotebookId = state.notebooks[0]?.id;
+            dispatch({ type: "setNotebookId", payload: firstNotebookId });
             setSearchParams({ notebookId: firstNotebookId });
-        }
+        } else dispatch({ type: "setNotebookId", payload: notebookId });
     }, [notebookId]);
 
     const createNotebook = (notebook: Notebook) => {
         notebookService.create(notebook);
-        getAllNotebooks();
+        dispatch({ type: "setNotebookId", payload: notebook.id });
+        dispatch({ type: "addNotebook", payload: notebook });
         setSearchParams({ notebookId: notebook.id });
     };
 
     const deleteNotebook = (id: string) => {
         notebookService.delete(id);
-        getAllNotebooks();
+        dispatch({ type: "deleteNotebook", payload: id });
     };
 
     const selectNotebook = (id: string) => {
+        dispatch({ type: "setNotebookId", payload: id });
         setSearchParams({ notebookId: id });
     };
 
     return (
         <NotebookContext.Provider
             value={{
-                notebooks,
+                ...state,
                 createNotebook,
-                notebookId,
                 deleteNotebook,
                 selectNotebook,
             }}
